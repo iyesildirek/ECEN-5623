919d601f2558afb516410b84810d5e27bb854170
iyesildirek <ismailyesildirek@gmail.com>
2019-07-21 15:38:00 -0500

    Created header file

--- Ex_4/Problem5/capture.c
+++ Ex_4/Problem5/capture.c
@@ -19,55 +19,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-
 #include <getopt.h>             /* getopt_long() */
-
 #include <fcntl.h>              /* low-level i/o */
 #include <unistd.h>
-#include <errno.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/time.h>
 #include <sys/mman.h>
 #include <sys/ioctl.h>
-
 #include <linux/videodev2.h>
-
 #include <time.h>
 
-#define CLEAR(x) memset(&(x), 0, sizeof(x))
-#define COLOR_CONVERT
-#define HRES 320
-#define VRES 240
-#define HRES_STR "320"
-#define VRES_STR "240"
-
-// Format is used by a number of functions, so made as a file global
-static struct v4l2_format fmt;
-
-enum io_method 
-{
-        IO_METHOD_READ,
-        IO_METHOD_MMAP,
-        IO_METHOD_USERPTR,
-};
-
-struct buffer 
-{
-        void   *start;
-        size_t  length;
-};
-
-static char            *dev_name;
-//static enum io_method   io = IO_METHOD_USERPTR;
-//static enum io_method   io = IO_METHOD_READ;
-static enum io_method   io = IO_METHOD_MMAP;
-static int              fd = -1;
-struct buffer          *buffers;
-static unsigned int     n_buffers;
-static int              out_buf;
-static int              force_format=1;
-static int              frame_count = 5;
+/*capture specific*/
+#include <errno.h>
+#include "capture.h"
 
 static void errno_exit(const char *s)
 {
@@ -87,9 +52,6 @@ static int xioctl(int fh, int request, void *arg)
 
         return r;
 }
-
-char ppm_header[]="P6\n#9999999999 sec 9999999999 msec \n"HRES_STR" "VRES_STR"\n255\n";
-char ppm_dumpname[]="pic000000.ppm";
 
 static void dump_ppm(const void *p, int size, unsigned int tag, struct timespec *time)
 {
@@ -160,11 +122,6 @@ void yuv2rgb(int y, int u, int v, unsigned char *r, unsigned char *g, unsigned c
    *g = g1 ;
    *b = b1 ;
 }
-
-
-
-unsigned int framecnt=0;
-unsigned char bigbuffer[(1280*960)];
 
 static void process_image(const void *p, int size)
 {
@@ -748,7 +705,6 @@ static void init_device(void)
     }
 }
 
-
 static void close_device(void)
 {
         if (-1 == close(fd))
@@ -798,21 +754,6 @@ static void usage(FILE *fp, int argc, char **argv)
                  "",
                  argv[0], dev_name, frame_count);
 }
-
-static const char short_options[] = "d:hmruofc:";
-
-static const struct option
-long_options[] = {
-        { "device", required_argument, NULL, 'd' },
-        { "help",   no_argument,       NULL, 'h' },
-        { "mmap",   no_argument,       NULL, 'm' },
-        { "read",   no_argument,       NULL, 'r' },
-        { "userp",  no_argument,       NULL, 'u' },
-        { "output", no_argument,       NULL, 'o' },
-        { "format", no_argument,       NULL, 'f' },
-        { "count",  required_argument, NULL, 'c' },
-        { 0, 0, 0, 0 }
-};
 
 int main(int argc, char **argv)
 {
---
+++ Ex_4/Problem5/capture.h
@@ -0,0 +1,74 @@
+#ifndef CAP
+#define CAP
+
+#define CLEAR(x) memset(&(x), 0, sizeof(x))
+#define COLOR_CONVERT
+#define HRES 320
+#define VRES 240
+#define HRES_STR "320"
+#define VRES_STR "240"
+
+/* Global Variables */
+// Format is used by a number of functions, so made as a file global
+static struct v4l2_format fmt;
+char ppm_header[]="P6\n#9999999999 sec 9999999999 msec \n"HRES_STR" "VRES_STR"\n255\n";
+char ppm_dumpname[]="pic000000.ppm";
+static char            *dev_name;
+static int              fd = -1;
+struct buffer          *buffers;
+static unsigned int     n_buffers;
+static int              out_buf;
+static int              force_format=1;
+static int              frame_count = 5;
+unsigned int framecnt=0;
+unsigned char bigbuffer[(1280*960)];
+static const char short_options[] = "d:hmruofc:";
+static const struct option
+long_options[] = {
+        { "device", required_argument, NULL, 'd' },
+        { "help",   no_argument,       NULL, 'h' },
+        { "mmap",   no_argument,       NULL, 'm' },
+        { "read",   no_argument,       NULL, 'r' },
+        { "userp",  no_argument,       NULL, 'u' },
+        { "output", no_argument,       NULL, 'o' },
+        { "format", no_argument,       NULL, 'f' },
+        { "count",  required_argument, NULL, 'c' },
+        { 0, 0, 0, 0 }
+};
+enum io_method 
+{
+        IO_METHOD_READ,
+        IO_METHOD_MMAP,
+        IO_METHOD_USERPTR,
+};
+
+//static enum io_method   io = IO_METHOD_USERPTR;
+//static enum io_method   io = IO_METHOD_READ;
+static enum io_method   io = IO_METHOD_MMAP;
+
+struct buffer 
+{
+        void   *start;
+        size_t  length;
+};
+
+/* Function Prototypes */
+static void errno_exit(const char *s);
+static int xioctl(int fh, int request, void *arg);
+static void dump_ppm(const void *p, int size, unsigned int tag, struct timespec *time);
+void yuv2rgb(int y, int u, int v, unsigned char *r, unsigned char *g, unsigned char *b);
+static void process_image(const void *p, int size);
+static int read_frame(void);
+static void mainloop(void);
+static void stop_capturing(void);
+static void start_capturing(void);
+static void uninit_device(void);
+static void init_read(unsigned int buffer_size);
+static void init_mmap(void);
+static void init_userp(unsigned int buffer_size);
+static void init_device(void);
+static void close_device(void);
+static void open_device(void);
+static void usage(FILE *fp, int argc, char **argv);
+
+#endif
